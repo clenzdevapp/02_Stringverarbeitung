eba7d0cbc4f9f8ed90293e0d63e8b1a6c63b48b8
mclenz2018 <41286288+mclenz2018@users.noreply.github.com>
2020-07-07 12:37:32 +0200

    Create 02_Strinverarbeitung.dart

---
+++ 02_Strinverarbeitung.dart
@@ -0,0 +1,9 @@
+main(){
+String meinName = "Justus";
+print ("Mein Name ist $meinName"); 
+double kommazahl = 3.5 ;
+String komma = kommazahl.toString();
+print (kommazahl);
+int laenge = meinName.length;
+print (meinName.length);
+}
